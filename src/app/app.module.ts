da32400364f618419c01410db6eabceeb0b4bd5f
emanoelcampos <emanoel.cv@gmail.com>
2022-10-27 11:39:56 -0300

    data form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
+import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent
+    TemplateFormComponent,
+    DataFormComponent
   ],
   imports: [
     BrowserModule,
