f086fc9a115f4eb0e937ad869bb1bfb0c092cb8b
emanoelcampos <emanoel.cv@gmail.com>
2022-10-28 11:40:03 -0300

    componente de debug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataFormComponent } from './data-form/data-form.component';
 import { TemplateFormModule } from './template-form/template-form.module';
+import { FormDebugComponent } from './form-debug/form-debug.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataFormComponent
+    DataFormComponent,
+    FormDebugComponent
   ],
   imports: [
     BrowserModule,
